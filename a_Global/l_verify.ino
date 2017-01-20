b212a33bc390a08a48fed68a3c15c702588eb93e
hasemar <hasemar@users.noreply.github.com>
2017-01-20 13:16:51 -0800

    Rename l_verify.ino to a_Global/l_verify.ino

---
+++ a_Global/l_verify.ino
@@ -0,0 +1,15 @@
+int verify(float vel)
+{
+  int result = 0; // output
+  float range = 5.0;   // use to adjust range of matching velocity to tune velocity comparison
+  if(vel > wtrVelocity - range && vel < wtrVelocity + range)
+  {
+    result = 0;
+  }
+  else
+  {
+    result = 1;
+  }
+  return result;
+}
+
