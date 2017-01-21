937828b4e63c11421ca0687e1b25863c0e8cca24
hasemar <ryan.haseman@stmartin.edu>
2017-01-20 23:38:18 -0800

    changed num var to const int
    changed const int num and also added a delay in the sampling to smooth
    it out a little

--- a_Global/i_levelRead.ino
+++ a_Global/i_levelRead.ino
@@ -6,16 +6,18 @@ int levelRead() // reads the capacitance level sensor and returns number of lase
   int avgRead;  // intermediate variable
   int maxLevel = 1023; // maximum sensor value (fully flooded) might change in the field to tune sensor
   int scaledCap = 0; // mapped reading for # of lasers
-  int samples[30]; // array for averaging sensor reading
+  const int num = 30;   // number of samples
+  int samples[num]; // array for averaging sensor reading
   capValue = 0; // start from zero
   
-  for(int i = 0; i<30; i++) //gather 30 samples and sum them up
+  for(int i = 0; i<num; i++) //gather 30 samples and sum them up
   {
     samples[i] = analogRead(levSnsrPin);
+    delay(5);
     capValue += samples[i];
   }
   
-  avgRead = capValue/30; // take average
+  avgRead = capValue/num; // take average
   scaledCap = map(avgRead, 0, maxLevel, 0, maxLasers); // should return # of lasers to use durring sweep
   capValue = analogRead(levSnsrPin);  // for debugging
   
