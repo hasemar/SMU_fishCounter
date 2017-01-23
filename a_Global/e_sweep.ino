d0e39d08a51cc6cf87c74f79a027a2ab1bc9b1fc
hasemar <hasemar@gmail.com>
2017-01-23 10:55:01 -0800

    eliminatedFeedback var
    I eliminated the global variable feedback. It was only used in sweep. I
    directly read put the recieverRead() return into the array element
    necessary

--- a_Global/e_sweep.ino
+++ a_Global/e_sweep.ino
@@ -3,33 +3,31 @@
  */
 void sweep(int arr[][laserColumns])
 {
+  int col1 = 0;   // column 1
+  int col2 = 1;   // column 2
   for(int i = 0; i<laserCount; i++)
   {
-    for(int j = 0; j<laserColumns; j++)
-    {
       digitalWrite(laser[i],HIGH); // fire laser for "cycle" ms
-      delay(fireTime);   // need this delay to give the sensor time to detect laser  (could subtract out for velocity calc)
-      feedback = receiverRead(j); // read reciever and return 1 or 0, for the jth column
+      delay(fireTime);   // delay to ensure that the laser is on before the input is read Also dictates cycle time
+      arr[i][col1] = receiverRead(col1); // read reciever and return 1 or 0, for the 1st column
+      Serial.print(photoresValue);
+      arr[i][col2] = receiverRead(col2);  // read reciever and return 1 or 0, for the 1st column
+      Serial.print(',');
       Serial.print(photoresValue);
       digitalWrite(laser[i],LOW);
-      
+      // debug stuff
       Serial.print(',');
-      Serial.print(feedback);
-      Serial.print(", [");
       Serial.print(i);
       Serial.print(',');
-      Serial.print(j);
-      Serial.print("] ,");
       Serial.print(timer);
       Serial.print(',');
       Serial.print(capValue);
       Serial.print(',');
       Serial.print(laserCount);
       Serial.print(',');
-      Serial.println(xCrit[j]);
-    
-    arr[i][j] = feedback; // feeds result into each element
-    } 
+      Serial.println(xCrit[col1]);
+      Serial.print(',');
+      Serial.println(xCrit[col2]);
   }
 }
 
